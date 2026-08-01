4f52a1ba7bc55d4427be39106c9b071784468cf3
payalpatil2510 <payalpatil.aesxb9@gmail.com>
2026-08-01 19:53:09 +0530

    Delete Assignment_7 directory

--- Assignment_7/Q6.py
+++
@@ -1,9 +0,0 @@
-n = 5
-
-for i in range(1, n + 1):
-    for j in range(i, n + 1):
-        if i == 1 or j == i or j == n:
-            print(j, end=" ")
-        else:
-            print("  ", end="")
-    print()
